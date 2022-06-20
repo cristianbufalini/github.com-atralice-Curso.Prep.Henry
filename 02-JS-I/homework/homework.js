50fe176c9a618777e558ccb97a9c140fcd6ba0c4
Cristian Adrian Bufalini <cristianbufaliniadrian@gmail.com>
2022-06-20 18:16:56 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "Hola";
+const nuevoNum = 50;
 
 // Crea una variable booleana:
 const nuevoBool = null;
